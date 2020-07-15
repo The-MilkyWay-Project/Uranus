6ce54e704af8386f0dd767e4495fc8fd6f33c7f2
Joshua walker <joshua.walker@sykescottages.co.uk>
2020-07-15 08:48:44 +0100

    Uranus

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,12 @@
+<script>
+import Tailwindcss from 'tres-2b/src/index.js';
+import Nav from 'kepler-4b/src/index.js';
+</script>
+
+<Tailwindcss />
+
+<Nav />
+
+<main>
+	<slot></slot>
+</main>
---
+++ src/routes/search.svelte
@@ -0,0 +1,21 @@
+<style>
+
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>project template</title>
+</svelte:head>
+
+<h1 class="text-blue-400">Uranus</h1>
